a6af9a59a4e60e14e1aed97b681bce846931cc8a
Loga Jegede <loga.jegede+github@gmail.com>
2012-08-08 00:13:39 +0100

    start blog tut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f4f9f5b1327e13f4ee9992db403cf6e955fd2f93e57a32798afab5ccacdbee0d2ef5c79482c087abe9d98d4fd28528ea37d469a98602129d9f18d52db2267532'
